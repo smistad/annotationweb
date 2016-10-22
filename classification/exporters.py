a681e99b6dcd61eb39ae02d546d816bf485269be
Erik Smistad <ersmistad@gmail.com>
2016-10-22 21:16:26 +0200

    Started on new exporter for cardiac data

--- classification/exporters.py
+++ classification/exporters.py
@@ -1,6 +1,6 @@
 from common.exporter import Exporter
 from common.utility import copy_image
-from annotationweb.models import ProcessedImage, Dataset, Task, Label
+from annotationweb.models import ProcessedImage, Dataset, Task, Label, Subject, ImageSequence
 from classification.models import ImageLabel
 from django import forms
 import os
@@ -98,4 +98,95 @@ class ClassificationExporter(Exporter):
 
         return True, path
 
+
+class CardiacExaminationsExporterForm(forms.Form):
+    path = forms.CharField(label='Storage path', max_length=1000)
+    delete_existing_data = forms.BooleanField(label='Delete any existing data at storage path', initial=False, required=False)
+
+    def __init__(self, task, data=None):
+        super().__init__(data)
+        self.fields['subjects_training'] = forms.ModelMultipleChoiceField(
+            queryset=Subject.objects.filter(dataset__task=task))
+        self.fields['subjects_validation'] = forms.ModelMultipleChoiceField(
+            queryset=Subject.objects.filter(dataset__task=task))
+
+
+class CardiacExaminationsExporter(Exporter):
+    """
+    This exporter will create a folder at the given path with two subfolders (training and validation)
+    In each subfolder 2 files are created:
+    labels.txt      - list of labels
+    file_list.txt   - list of files and labels
+    A folder is created for each dataset with the actual images
+    """
+
+    task_type = Task.CLASSIFICATION
+    name = 'Cardiac examinations classification exporter'
+
+    def get_form(self, data=None):
+        return CardiacExaminationsExporterForm(self.task, data=data)
+
+    def export(self, form):
+        delete_existing_data = form.cleaned_data['delete_existing_data']
+        # Create dir, delete old if it exists
+        path = form.cleaned_data['path']
+        if delete_existing_data:
+            # Delete path
+            try:
+                os.stat(path)
+                rmtree(path)
+            except:
+                # Folder does not exist
+                pass
+
+            # Create folder again
+            try:
+                os.mkdir(path)
+            except:
+                return False, 'Failed to create directory at ' + path
+        else:
+            # Check that folder exist
+            try:
+                os.stat(path)
+            except:
+                return False, 'Path does not exist: ' + path
+
+
+        # Create label file
+        label_file = open(os.path.join(path, 'labels.txt'), 'w')
+        labels = Label.objects.filter(task=self.task)
+        labelDict = {}
+        counter = 0
+        for label in labels:
+            label_file.write(label.name + '\n')
+            labelDict[label.name] = counter
+            counter += 1
+        label_file.close()
+
+        # Create file_list.txt file
+        # TODO FINISH
+        datasets = form.cleaned_data['dataset']
+        file_list = open(os.path.join(path, 'file_list.txt'), 'w')
+        labeled_images = ProcessedImage.objects.filter(task=self.task, image__dataset__in=datasets)
+        for labeled_image in labeled_images:
+            name = labeled_image.image.filename
+            dataset_path = os.path.join(path, labeled_image.image.dataset.name)
+            try:
+                os.mkdir(dataset_path) # Make dataset path if doesn't exist
+            except:
+                pass
+
+            image_id = labeled_image.image.id
+            new_extension = form.cleaned_data['output_image_format']
+            new_filename = os.path.join(dataset_path, str(image_id) + '.' + new_extension)
+            copy_image(name, new_filename)
+
+            # Get image label
+            label = ImageLabel.objects.get(image=labeled_image)
+
+            file_list.write(new_filename + ' ' + str(labelDict[label.label.name]) + '\n')
+
+        file_list.close()
+
+        return True, path
 
