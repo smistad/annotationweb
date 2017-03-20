c884895c7871d680b79f65e473ed941915905ff6
Erik Smistad <ersmistad@gmail.com>
2017-03-20 13:00:50 +0100

    Modified segmentation exporter

--- exporters/segmentation_exporter.py
+++ exporters/segmentation_exporter.py
@@ -1,6 +1,6 @@
 from common.exporter import Exporter
 from common.utility import copy_image
-from annotationweb.models import ProcessedImage, Dataset, Task, Label
+from annotationweb.models import ProcessedImage, Dataset, Task, Label, KeyFrame
 from django import forms
 import os
 from shutil import rmtree, copyfile
@@ -61,14 +61,28 @@ class SegmentationExporter(Exporter):
             except:
                 pass
 
+            # TODO make sequence path if it doesn't exists
+            final_slash = name.rfind('/')
+            second_slash = name.rfind('/', 0, final_slash-1)
+            dataset_name = name[second_slash+1:final_slash]
+            dataset_path = os.path.join(subject_path, dataset_name)
+            print(dataset_path)
+            try:
+                os.mkdir(dataset_path)
+            except:
+                pass
+
+            # Get key frame nr
+            key_frame = KeyFrame.objects.get(image=segmented_image.image)
+            frame_nr = key_frame.frame_nr
+
             # Copy image
-            image_id = segmented_image.image.pk
-            new_filename = os.path.join(subject_path, str(image_id) + '.mhd')
+            new_filename = os.path.join(dataset_path, str(frame_nr) + '.mhd')
             copy_image(name, new_filename)
 
             # Copy all segmentation files
             segmentation_filename = os.path.join(BASE_DIR, os.path.join('segmentations', os.path.join(str(self.task.id), str(segmented_image.id) + '.mhd')))
-            new_segmentation_filename = os.path.join(subject_path, str(image_id) + '_segmentation.mhd')
+            new_segmentation_filename = os.path.join(dataset_path, str(frame_nr) + '_segmentation.mhd')
             copy_image(segmentation_filename, new_segmentation_filename)
 
 
