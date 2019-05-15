6975a061a82168b8760005bb9c2827c7cd45d77c
Erik Smistad <ersmistad@gmail.com>
2019-05-15 15:38:57 +0200

    Fixed login middleware and login for django 2.2

--- annotationweb/login.py
+++ annotationweb/login.py
@@ -6,28 +6,21 @@ EXEMPT_URLS = [compile(settings.LOGIN_URL.lstrip('/'))]
 if hasattr(settings, 'LOGIN_EXEMPT_URLS'):
     EXEMPT_URLS += [compile(expr) for expr in settings.LOGIN_EXEMPT_URLS]
 
-class LoginRequiredMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+from django.utils.deprecation import MiddlewareMixin
 
-    def __call__(self, request):
-        return self.get_response(request)
+class LoginRequiredMiddleware(MiddlewareMixin):
     """
     Middleware that requires a user to be authenticated to view any page other
     than LOGIN_URL. Exemptions to this requirement can optionally be specified
     in settings via a list of regular expressions in LOGIN_EXEMPT_URLS (which
     you can copy from your urls.py).
-    Requires authentication middleware and template context processors to be
-    loaded. You'll get an error if they aren't.
     """
     def process_request(self, request):
-        assert hasattr(request, 'user'), "The Login Required middleware\
- requires authentication middleware to be installed. Edit your\
- MIDDLEWARE_CLASSES setting to insert\
- 'django.contrib.auth.middleware.AuthenticationMiddleware'. If that doesn't\
- work, ensure your TEMPLATE_CONTEXT_PROCESSORS setting includes\
- 'django.core.context_processors.auth'."
-        if not request.user.is_authenticated():
+        assert hasattr(request, 'user'), """
+        The Login Required middleware needs to be after AuthenticationMiddleware.
+        Also make sure to include the template context_processor:
+        'django.contrib.auth.context_processors.auth'."""
+        if not request.user.is_authenticated:
             path = request.path_info.lstrip('/')
             if not any(m.match(path) for m in EXEMPT_URLS):
                 return HttpResponseRedirect(settings.LOGIN_URL)
--- user/views.py
+++ user/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, views
 
-# Create your views here.
+
 def login(request):
-    return views.login(request, template_name='user/login.html')
+    return views.LoginView.as_view(template_name='user/login.html')(request)
+
 
 def logout(request):
     return views.logout_then_login(request)
