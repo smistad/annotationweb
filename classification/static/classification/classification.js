03b4475df1735fb74d873bcc9857ab4b2558f97e
androst <a.ostvik@gmail.com>
2019-11-12 10:16:11 +0100

    - Included new dictionary for storing target_frames with corresponding labels. - Avoid auto save when clicking on class if individual frames are classified. - Override functionality on image loaded signal (progressbar). - Override slidermark update and changeLabel to work with new functionality.

--- classification/static/classification/classification.js
+++ classification/static/classification/classification.js
@@ -1,3 +1,4 @@
+var g_targetLabels = {}; // dictionary [target_frame_nr] contains a dictionary/map of frames with corresponding label
 
 function sendDataForSave() {
     return $.ajax({
@@ -7,6 +8,8 @@ function sendDataForSave() {
             image_id: g_imageID,
             task_id: g_taskID,
             label_id: g_currentLabel,
+            target_frames: JSON.stringify(g_targetFrames),
+            target_labels: JSON.stringify(g_targetLabels),
             quality: $('input[name=quality]:checked').val(),
             rejected: g_rejected ? 'true':'false',
             comments: $('#comments').val(),
@@ -27,21 +30,104 @@ function loadClassificationTask() {
         changeLabel(-1);
     });
 
-    // Add click listener for label buttons to trigger save
-    for(var i = 0; i < g_labelButtons.length; ++i) {
-        var label_id = g_labelButtons[i].id;
-        $('#labelButton' + label_id).click(function() {
-            // Only trigger save if label has no children
-            var childrenFound = false;
-            for(var j = 0; j < g_labelButtons.length; j++) {
-                var child_label = g_labelButtons[j];
-                if(child_label.parent_id == g_currentLabel) {
-                    childrenFound = true;
-                } else if(child_label.parent_id != 0) {
+    // Add click listener for label buttons to trigger save if annotating whole sequence with one label
+    if(!g_annotateIndividualFrames){
+        for(var i = 0; i < g_labelButtons.length; ++i) {
+            var label_id = g_labelButtons[i].id;
+            $('#labelButton' + label_id).click(function() {
+                // Only trigger save if label has no children
+                var childrenFound = false;
+                for(var j = 0; j < g_labelButtons.length; j++) {
+                    var child_label = g_labelButtons[j];
+                    if(child_label.parent_id == g_currentLabel) {
+                        childrenFound = true;
+                    } else if(child_label.parent_id != 0) {
+                    }
                 }
-            }
-            if(!childrenFound)
-                save();
-        });
+                if(!childrenFound)
+                    save();
+            });
+        }
     }
+
+    $('#addFrameButton').click(function() {
+        addLabelsForNewFrame(g_currentFrameNr);
+    });
+
+    // Overload of progressbar complete in annotationweb.js
+    $('#progressbar').progressbar({
+        complete: function () {
+            $(".progress-label").text( "Finished loading!" );
+            g_progressbar.hide();
+            redrawSequence();
+
+            for(label in g_targetLabels)
+                updateSliderMark(label, g_targetLabels[label]);
+
+            if(g_isPlaying)
+                incrementFrame();
+        }
+    });
+}
+
+function addLabelsForNewFrame(frameNr) {
+    if(frameNr in g_targetLabels) // Already exists
+        return;
+    g_targetLabels[frameNr] = {};
+}
+
+// Overload of annotationweb.js implementation of setupSlideMark
+function updateSliderMark(frame, label){
+    var frame = parseInt(frame);
+
+    var marker_index = g_targetFrames.findIndex(index => index === frame);
+    var label = label.label
+    var newMarker = document.createElement("sliderMarker" + marker_index)
+
+    $(newMarker).css('background-color', colorToHexString(label.red, label.green, label.blue));
+    $(newMarker).css('width', $('.ui-slider-handle').css('width'));
+    $(newMarker).css('margin-left', $('.ui-slider-handle').css('margin-left'));
+    $(newMarker).css('height', '100%');
+    $(newMarker).css('z-index', '99');
+    $(newMarker).css('position', 'absolute');
+    $(newMarker).css('left', ''+(100.0*(frame-g_startFrame)/g_framesLoaded)+'%');
+    slider.appendChild(newMarker)
+}
+
+function loadLabels(target_frame, label_id) {
+    for(var i = 0; i < g_labelButtons.length; i++) {
+        if (g_labelButtons[i].id == label_id) {
+            var label = g_labelButtons[i];
+            g_targetLabels[target_frame] = {label: label};
+            break;
+        }
+    }
+}
+
+// Override of annotationweb.js
+function changeLabel(label_id) {
+    for(var i = 0; i < g_labelButtons.length; i++)  {
+        if(g_labelButtons[i].id == label_id) {
+
+            // Hide previous label's sublabels and show new ones
+            var labelList = getLabelList(g_labelButtons[i]);
+            showLabelList(labelList);
+
+            g_currentLabel = label_id;
+            var label = g_labelButtons[i];
+
+            // Set correct button to active
+            if(!g_annotateIndividualFrames)
+                decorateLabelButtons(labelList);
+
+            g_currentColor = {red: label.red, green: label.green, blue: label.blue};
+
+            console.log(i + ' is now active label for frame ' + g_currentFrameNr);
+            g_targetLabels[g_currentFrameNr] = {label: label};
+            break;
+        }
+    }
+
+    for(label in g_targetLabels)
+        updateSliderMark(label, g_targetLabels[label]);
 }
