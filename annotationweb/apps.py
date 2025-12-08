a91167c2a59093614d370a05ecbe298cf9ae14fb
Erik Smistad <ersmistad@gmail.com>
2025-12-08 14:16:46 +0100

    Show AW version in footer

--- annotationweb/apps.py
+++ annotationweb/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class AnnotationwebConfig(AppConfig):
     name = 'annotationweb'
+    SITE_NAME = 'Annotation Web'
+    # Annotation Web version
+    VERSION_MAJOR = 2
+    VERSION_MINOR = 0
+    VERSION_PATCH = 0
+    VERSION = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
