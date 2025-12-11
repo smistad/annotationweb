eddd148d6133fd4c4740beec56c115eeb31b4d56
Erik Smistad <ersmistad@gmail.com>
2025-12-11 15:24:16 +0100

    Added serialization feature to more apps

--- cardiac_parasternal_long_axis/models.py
+++ cardiac_parasternal_long_axis/models.py
@@ -1,1 +1,1 @@
-
+# This app uses the model in the spline_segmentation app
--- cardiac_parasternal_long_axis/serialization.py
+++ cardiac_parasternal_long_axis/serialization.py
@@ -1,1 +1,2 @@
 # This app uses the model in the spline_segmentation app
+from spline_segmentation.serialization import serialize
